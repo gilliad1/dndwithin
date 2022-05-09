13e0679ae9d135d83e2fc43c7747fd7ce6bf6208
z-proy <z-proy@zebralabs.org>
2022-05-09 16:06:36 -0400

    add characters

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE public.spells (
+CREATE TABLE IF NOT EXISTS public.spells (
     id integer NOT NULL,
     name text,
     school character varying(25),
@@ -22,3 +22,41 @@ ALTER TABLE ONLY public.spells ADD CONSTRAINT spells_pkey PRIMARY KEY (id);
 REVOKE CONNECT,TEMPORARY ON DATABASE dndwithin FROM PUBLIC;
 REVOKE ALL ON DATABASE dndwithin FROM "svc-dndwithin";
 GRANT ALL ON DATABASE dndwithin TO "svc-dndwithin" WITH GRANT OPTION;
+
+CREATE TABLE IF NOT EXISTS public.characters (
+    id integer NOT NULL,
+    name text,
+    hitPoints integer,
+    speed integer,
+    ac integer,
+    touchAC integer,
+    flatFootAC integer,
+    classes text,
+    strength integer,
+    dexterity integer,
+    constitution integer,
+    intelligence integer,
+    wisdom integer,
+    charisma integer,
+    initiativeMod integer,
+    fortitudeSave integer,
+    reflexSave integer,
+    willSave integer,
+    attackHandheld integer,
+    attackMissile integer,
+    grappleCheck integer,
+    carryingLight integer,
+    carryingMedium integer,
+    carryingHeavy integer,
+    carryLiftOverhead integer,
+    carryLiftOffGrouhd integer,
+    carryPushDrag integer,
+    languages text,
+    feats text,
+    skills text,
+    background text,
+    description text,
+    mostRecent boolean DEFAULT true
+)
+ALTER TABLE public.characters OWNER TO "svc-dndwithin";
+ALTER TABLE ONLY public.characters ADD CONSTRAINT characters_pkey PRIMARY KEY (id);
